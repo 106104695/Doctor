628faeec3afb8415cf89112828e0f8c437f496f7
106104695@qq.com <106104695@qq.com>
2016-12-31 11:42:21 +0800

    1111

--- app/src/main/java/com/doctor/demo/fragment/HomeFragment.java
+++ app/src/main/java/com/doctor/demo/fragment/HomeFragment.java
@@ -10,9 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 import com.doctor.demo.R;
+import com.doctor.demo.view.UPMarqueeView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
     private ViewPager mPager;
@@ -27,7 +33,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     private boolean isFoucusRight; // ScrollView是否滚动到右侧
     private View layout;
 
-
+    private UPMarqueeView upview1;
+    List<String> data = new ArrayList<>();
+    List<View> upviews = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +49,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         initView();
         initPager();
         autoScroll();
-
+        initParam();
+        initdata();
+        initupView();
         return layout;
     }
 
@@ -51,12 +61,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         mImageView = (ImageView) layout.findViewById(R.id.img_indicator01);
 
     }
-
-   /* private void activeCategory() {
-        MainActivity activity = (MainActivity) getActivity();
-        activity.activeCategory();
-    }*/
-
 
     @Override
     public void onDestroyView() {
@@ -92,14 +96,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 mPager.postDelayed(this, 3000);
             }
         }, 3000);
-
-
-
-
-
-
-
-
 
  /*   @Override
     public void onClick(View v) {
@@ -155,7 +151,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
 
     }
-
     private class MyPagerAdapter  extends FragmentStatePagerAdapter {
 
         public MyPagerAdapter(FragmentManager fm) {
@@ -225,4 +220,89 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             }
         }
     }
+
+
+    /**
+     * 实例化控件
+     */
+    private void initParam() {
+        upview1 = (UPMarqueeView) layout.findViewById(R.id.upview1);
+    }
+
+    /**
+     * 初始化数据
+     */
+    private void initdata() {
+        data = new ArrayList<>();
+        data.add("家人给2岁孩子喝这个，孩子智力倒退10岁!!!");
+        data.add("iPhone8最感人变化成真，必须买买买买!!!!");
+        data.add("简直是白菜价！日本玩家33万甩卖15万张游戏王卡");
+        data.add("iPhone7价格曝光了！看完感觉我的腰子有点疼...");
+        data.add("主人内疚逃命时没带够，回废墟狂挖30小时！");
+        data.add("竟不是小米乐视！看水抢了骁龙821首发了！！！");
+
+    }
+    /**
+     * 初始化界面程序
+     */
+    private void initupView() {
+        setView();
+        upview1.setViews(upviews);
+        /**
+         * 设置item_view的监听
+         */
+        upview1.setOnItemClickListener(new UPMarqueeView.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+
+            }
+        });
+    }
+
+    /**
+     * 初始化需要循环的View
+     * 为了灵活的使用滚动的View，所以把滚动的内容让用户自定义
+     * 假如滚动的是三条或者一条，或者是其他，只需要把对应的布局，和这个方法稍微改改就可以了，
+     */
+    private void setView() {
+        for (int i = 0; i < data.size(); i = i + 2) {
+            final int position = i;
+            //设置滚动的单个布局
+            LinearLayout moreView = (LinearLayout) LayoutInflater.from(getActivity()).inflate(R.layout.item_upview, null);
+            //初始化布局的控件
+            TextView tv1 = (TextView) moreView.findViewById(R.id.tv1);
+            TextView tv2 = (TextView) moreView.findViewById(R.id.tv2);
+
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            /**
+             * 设置监听
+             */
+            moreView.findViewById(R.id.rl2).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+            //进行对控件赋值
+            tv1.setText(data.get(i).toString());
+            if (data.size() > i + 1) {
+                //因为淘宝那儿是两条数据，但是当数据是奇数时就不需要赋值第二个，所以加了一个判断，还应该把第二个布局给隐藏掉
+                tv2.setText(data.get(i + 1).toString());
+            } else {
+                moreView.findViewById(R.id.rl2).setVisibility(View.GONE);
+            }
+
+            //添加到循环滚动数组里面去
+            upviews.add(moreView);
+        }
+    }
+
 }
